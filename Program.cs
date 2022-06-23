de811cbff62d949e847c180811b239c0088442a0
Tomas Manhal <info@tomasmanhal.cz>
2022-06-23 20:06:28 +0200

    aktualizace bota - autoresponse

--- Program.cs
+++ Program.cs
@@ -1,2 +1,79 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using Discord;
+using Discord.Net;
+using Discord.Commands;
+using Discord.WebSocket;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.Json;
+
+namespace csharpi
+{
+    class Program
+    {
+        private readonly DiscordSocketClient _client;
+        private readonly IConfiguration _config;
+
+        public static Task Main(string[] args) => new Program().MainAsync();
+
+        public async Task MainAsync(string[] args)
+        {
+            
+        }
+
+        public Program()
+        {
+            _client = new DiscordSocketClient();
+
+            //Hook into log event and write it out to the console
+            _client.Log += Log;
+
+            //Hook into the client ready event
+            _client.Ready += Ready;
+
+            //Hook into the message received event, this is how we handle the hello world example
+            _client.MessageReceived += MessageReceivedAsync;
+
+            //Create the configuration
+            var _builder = new ConfigurationBuilder()
+                .SetBasePath(AppContext.BaseDirectory)
+                .AddJsonFile(path: "config.json");            
+            _config = _builder.Build();
+        }
+
+        public async Task MainAsync()
+        {
+            //This is where we get the Token value from the configuration file
+            await _client.LoginAsync(TokenType.Bot, _config["Token"]);
+            await _client.StartAsync();
+
+            // Block the program until it is closed.
+            await Task.Delay(-1);
+        }
+
+        private Task Log(LogMessage log)
+        {
+            Console.WriteLine(log.ToString());
+            return Task.CompletedTask;
+        }
+
+        private Task Ready()
+        {
+            Console.WriteLine($"Connected as -> [] :)");
+            return Task.CompletedTask;
+        }
+
+        //I wonder if there's a better way to handle commands (spoiler: there is :))
+        private async Task MessageReceivedAsync(SocketMessage message)
+        {
+            //This ensures we don't loop things by responding to ourselves (as the bot)
+            if (message.Author.Id == _client.CurrentUser.Id)
+                return;
+
+            if (message.Content == ".hello")
+            {
+                await message.Channel.SendMessageAsync("world!");
+            }  
+        }
+    }
+}
